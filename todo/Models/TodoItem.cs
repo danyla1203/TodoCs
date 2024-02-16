9926d42aa617f9b491d5eb9c3a94c39136b7164c
Daniil <zelenko.danil@gmail.com>
2024-02-16 17:37:56 +0100

    validation for TodoItem

--- todo/Models/TodoItem.cs
+++ todo/Models/TodoItem.cs
@@ -1,8 +1,12 @@
-﻿namespace todo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+namespace todo.Models;
 
 public class TodoItem
 {
     public long Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
