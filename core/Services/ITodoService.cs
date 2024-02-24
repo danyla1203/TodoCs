d9ae174b47897069a029352157b470491513fbc5
Daniil <zelenko.danil@gmail.com>
2024-02-24 01:36:52 +0100

    todo service throw client error or return value

--- core/Services/ITodoService.cs
+++ core/Services/ITodoService.cs
@@ -1,3 +1,4 @@
+using todo.Data.Dto;
 using todo.Models;
 
 namespace todo.Services;
@@ -5,7 +6,7 @@ namespace todo.Services;
 public interface ITodoService
 {
   Task<List<TodoItem>> GetTodoList(bool? completed);
-  Task<TodoItem?> GetTodoItem(int id);
-  Task<TodoItem> AddTodoItem(TodoItem todoItem);
-  Task<TodoItem?> DeleteTodoItem(int id);
+  Task<TodoItem> GetTodoItem(int id);
+  Task<TodoItem> AddTodoItem(TodoItemDto todoItem);
+  Task<TodoItem> DeleteTodoItem(int id);
 }
--- core/Services/TodoService.cs
+++ core/Services/TodoService.cs
@@ -1,4 +1,5 @@
-﻿using todo.Exceptions.TodoExceptions;
+﻿using todo.Data.Dto;
+using todo.Exceptions.TodoExceptions;
 using todo.Models;
 
 namespace todo.Services;
@@ -19,19 +20,23 @@ public class TodoService : ITodoService
             todo => todo.IsComplete == completed : null
         );
     }
-
-    public async Task<TodoItem?> GetTodoItem(int id)
+    public async Task<TodoItem> GetTodoItem(int id)
     {
         TodoItem? item = await _unit.TodoRepository.GetById(id);
-        return item ?? throw new TodoItemNotFound(); 
+        return item ?? throw new TodoItemNotFound();
     }
-    public async Task<TodoItem> AddTodoItem(TodoItem todoItem)
+    public async Task<TodoItem> AddTodoItem(TodoItemDto todoItem)
     {
-        await _unit.TodoRepository.AddItem(todoItem);
+        var item = new TodoItem
+        {
+            Name = todoItem.Name,
+            IsComplete = todoItem.IsComplete ?? false
+        };
+        var newRecord = await _unit.TodoRepository.AddItem(item);
         await _unit.Save();
-        return todoItem;
+        return newRecord;
     }
-    public async Task<TodoItem?> DeleteTodoItem(int id)
+    public async Task<TodoItem> DeleteTodoItem(int id)
     {
         TodoItem? record = await _unit.TodoRepository.GetById(id);
         if (record == null) throw new TodoItemNotFound();
@@ -39,5 +44,5 @@ public class TodoService : ITodoService
         await _unit.TodoRepository.Delete(id);
         await _unit.Save();
         return record;
-    } 
+    }
 }
