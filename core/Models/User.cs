41b92595316fba65b424405255990f0571daaaa1
Daniil <zelenko.danil@gmail.com>
2024-02-27 00:32:05 +0100

    Email is unique

--- core/Models/User.cs
+++ core/Models/User.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace todo.Models;
 
+[Index(nameof(Email), IsUnique = true)]
 public class User
 {
     public long Id { get; set; }
